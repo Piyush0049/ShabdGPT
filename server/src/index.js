c47372b0cc846e67f96babb9d8a71cadf5c09966
piyush0049 <piyushjoshi81204@gmail.com>
2025-08-05 21:39:35 +0530

    code cleaning

--- server/src/index.js
+++ server/src/index.js
@@ -9,19 +9,15 @@ dotenv.config();
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 
-// Initialize express app
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
