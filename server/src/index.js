63c7c6bc82beede8c06f8f1db8cbd4a5ac9f7f2d
piyush0049 <piyushjoshi81204@gmail.com>
2025-08-06 21:45:10 +0530

    code cleaning

--- server/src/index.js
+++ server/src/index.js
@@ -30,7 +30,6 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     
-    // Start server
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
