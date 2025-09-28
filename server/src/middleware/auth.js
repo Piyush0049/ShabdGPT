5b1e26729c51790f678a05868a3307a305a6e789
piyush0049 <piyushjoshi81204@gmail.com>
2025-09-28 22:40:57 +0530

    changes

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -17,7 +17,6 @@ exports.protect = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // Add user to req object
     req.user = await User.findById(decoded.id);
 
     next();
